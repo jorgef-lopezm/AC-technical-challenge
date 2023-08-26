58609dd61ade88ca3b6e4cd21177bb1353d14cdd
jorgef-lopezm <jorgef.lopezm@gmail.com>
2023-08-26 15:53:32 -0600

    adding bastion sg

--- vpc/main.tf
+++ vpc/main.tf
@@ -131,7 +131,10 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_sg.id]
+    security_groups = [
+      aws_security_group.ecs_sg.id, 
+      aws_security_group.bastion_sg.id
+    ]
   }
 
   tags = local.common_tags
@@ -160,6 +163,42 @@ resource "aws_security_group" "ecs_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
+    cidr_blocks = var.private_subnets
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_security_group" "bastion_sg" {
+  description = "Segurity group for bastion instance"
+  name        = "${local.prefix}-bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
     cidr_blocks = var.private_subnets
   }
 
