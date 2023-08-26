b09bbbc766a5f174cfb844d42391d7574f8747db
jorgef-lopezm <jorgef.lopezm@gmail.com>
2023-08-26 15:44:01 -0600

    refactoring ecs

--- ecs/main.tf
+++ ecs/main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "task_execution_role" {
   tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
   role       = aws_iam_role.task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
