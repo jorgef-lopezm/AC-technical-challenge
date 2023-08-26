fdc1e21efe411398662909830ec508dbabd84920
jorgef-lopezm <jorgef.lopezm@gmail.com>
2023-08-26 15:44:45 -0600

    setting up data.tf for ec2

---
+++ ec2/data.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
+}
